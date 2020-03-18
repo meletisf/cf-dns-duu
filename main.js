957a8cc407c897c5dc5485a8c4e3a8186b1745ca
Meletis Flevarakis <fmeletis85@gmail.com>
2020-03-19 01:03:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,11 @@ const log = require('./helpers/LogHelper');
 const IPProvider = require('./helpers/IPProvidersHelper');
 const Cloudflare = require('./helpers/CloudflareHelper');
 
+console.log(config)
 log.info(`Ready to update ${config.cloudflare.subdomain} (${config.cloudflare.record})`)
 
 IPProvider[config.provider]()
-    .then(Cloudflare.updateIP() 
-            .catch(e => {
-                log.error(e)
-            }))
+    .then(Cloudflare.updateIP)
     .catch(e => {
         log.error(e)
     });
